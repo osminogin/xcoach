14863cc5630ed103fa0352be0d6dfda3cdd7dd51
Vladimir Osintsev <osintsev@gmail.com>
2016-01-30 01:58:08 +0300

    Use timezone func from django.utils instead datetime Add related_bets and total vars to context

--- apps/events/views.py
+++ apps/events/views.py
@@ -1,12 +1,22 @@
-from datetime import datetime
+from django.utils import timezone
+from django.db.models import Sum
 from django.views.generic import ListView, DetailView
+from lib.teams import find_target_team
+from apps.bets.models import Bet
 from .models import Event
 
 
 class EventList(ListView):
     model = Event
-    queryset = Event.objects.filter(start_time__gte=datetime.now())
+    queryset = Event.objects.filter(start_time__gte=timezone.now())
 
 
 class EventDetail(DetailView):
     model = Event
+
+    def get_context_data(self, **kwargs):
+        context = super(EventDetail, self).get_context_data(**kwargs)
+        target = find_target_team(context['event'].title)
+        context['related_bets'] = Bet.objects.filter(event__title__icontains=target)
+        context['total'] = context['related_bets'].aggregate(Sum('balance'))
+        return context
